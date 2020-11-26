640ff3235dcdb34a41c1c151963bb55b9b055878
Kamyar Pakdaman <57539668+kamyarpakdaman@users.noreply.github.com>
2020-11-27 00:15:25 +0330

    Update titanic_survival.py

--- SupervisedLearning/titanic_survival/titanic_survival.py
+++ SupervisedLearning/titanic_survival/titanic_survival.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
